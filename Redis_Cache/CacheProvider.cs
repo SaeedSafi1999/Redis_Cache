dff9aee323e08b7b11476fabc4288078c13a84be
saeedsafi <safi.saeed1999@gmail.com>
2023-03-03 14:06:29 +0330

    Initial commit

---
+++ Redis_Cache/CacheProvider.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Redis_Cache;
+
+public class CacheProvider : ICacheprovider
+{
+    private readonly IDistributedCache _cache;
+
+    public CacheProvider(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<T> GetAsync<T>(string key) where T : class
+    {
+        var data = await _cache.GetStringAsync(key);
+        return data == null ? null : JsonSerializer.Deserialize<T>(data);
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan time) where T : class
+    {
+        DistributedCacheEntryOptions options  = new DistributedCacheEntryOptions()
+            .SetAbsoluteExpiration(time);
+
+        var data = JsonSerializer.Serialize(value);
+        await _cache.SetStringAsync(key, data, options);
+    }
+
+    public async Task ClearAsync(string key)
+    {
+        await _cache.RemoveAsync(key);
+    }
+
+
+    public bool TryGetValue<T>(string key, out T result)
+    {
+        var json = _cache.GetString(key);
+        if (json == null)
+        {
+            result = default(T);
+            return false;
+        }
+
+        result = JsonSerializer.Deserialize<T>(json);
+        return true;
+    }
+
+}
---
+++ Redis_Cache/ICacheprovider.cs
@@ -0,0 +1,9 @@
+﻿namespace Redis_Cache;
+
+public interface ICacheprovider
+{
+    Task<T> GetAsync<T>(string key) where T : class;
+    Task SetAsync<T>(string key, T value, TimeSpan time) where T : class;
+    Task ClearAsync(string key);
+    bool TryGetValue<T>(string key, out T result);
+}
